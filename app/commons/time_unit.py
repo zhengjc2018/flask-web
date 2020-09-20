5d7a1411f919513e1b4bd08d26395c70b1ce9cee
zhengjc2018 <zhengjc1995@gamil>
2020-09-20 23:36:56 +0800

    update

--- app/commons/time_unit.py
+++ app/commons/time_unit.py
@@ -1,4 +1,5 @@
-from time import time
+import time
+import datetime
 
 
 class TimesUnit:
@@ -6,10 +7,14 @@ class TimesUnit:
     def get_now(Accuracy=13):
         # Accuracy为精确度
         if Accuracy == 13:
-            return int(time() * 1000)
-        return int(time())
+            return int(time.time() * 1000)
+        return int(time.time())
+
+    def get_first_day_of_month(year, month):
+        return int(time.mktime(datetime.date(year, month, 1).timetuple()))
 
 
 if __name__ == '__main__':
     a = TimesUnit.get_now()
     print(a)
+    print(TimesUnit.get_first_day_of_month(2020, 9))
