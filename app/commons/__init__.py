6ff91037586cae7a00e669c7caa9e64ead8673ff
zhengjc2018 <zhengjc1995@gamil>
2020-10-24 13:42:37 +0800

    update

--- app/commons/__init__.py
+++ app/commons/__init__.py
@@ -1,7 +1,8 @@
 from .time_unit import TimesUnit
 from .excelUtil import ExcelUtils
 from .wordUtil import WordUtils
+from .zip import make_zip
 
 __all__ = [
-    TimesUnit, ExcelUtils, WordUtils
+    TimesUnit, ExcelUtils, WordUtils, make_zip
 ]
